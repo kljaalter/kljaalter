919e655f45477a81e6a0a05dfd0cd86ffdfcbc0d
woutcorijn <26119233+woutcorijn@users.noreply.github.com>
2020-10-04 12:29:45 +0200

    V3.75

--- js.js
+++ js.js
@@ -1,2 +1,11 @@
+Year = [2008,2012,2013,2014,2015,2016,2017,2018,2019];
+Count = 0;
 
-console.log("KLJ Aalter")
+setInterval(function() {
+Count++
+if (Count == 9) {
+  Count = 0;
+};
+
+document.getElementById("fotomaand").src = "images/Foto_Van_De_Maand/" + Year[Count] + ".png";
+}, 1500);
